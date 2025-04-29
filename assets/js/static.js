829c947311cc0a30d6a80c831ad7c64482e2f4d7
mataager <omarbussmo077@gmail.com>
2025-04-29 13:24:21 +0300

    Add files via upload

--- assets/js/static.js
+++ assets/js/static.js
@@ -1,8 +1,8 @@
 const url = "https://matager-f1f00-default-rtdb.firebaseio.com";
 const storeurl = "https://matager-f1f00-default-rtdb.firebaseio.com";
 const storename = "King store";
-const uid = "GxQaBxk8tFa25RudRDnpAUyjQ0l1";
-const storeuid = "GxQaBxk8tFa25RudRDnpAUyjQ0l1";
+const uid = "yiqbf0f6r6PSQUjE6eAtXWc2wRu1";
+const storeuid = "yiqbf0f6r6PSQUjE6eAtXWc2wRu1";
 const storePhoneNum = "+20 106 530 6546";
 const storePhoneNum2 = "";
 const storeEmail = "mostafamohamed01153338870@gmail.mo";
@@ -11,14 +11,14 @@ const storeGender = ["Men"];
 const productnavbar = ["no"];
 const brandsnavbar = ["no"];
 const storeType = "clothing"; // clothing or footwear
-const freeshipping = "200";
+const freeshipping = "2000";
 const minshipping = 65;
 const maxshipping = 90;
 const specialshipping = 75;
 const vanishedstock = false;
 
 // guest checkout
-const GuestEmail = "exampleguest999@gmail.com";
+const GuestEmail = "kingstoreguest@gmail.com";
 //images url
 const pannerurl =
   "https://images.unsplash.com/photo-1731983061288-a851eb9c9cb7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
