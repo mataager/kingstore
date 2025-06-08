798c570b7ce76ad4bbf55f3dae9581e04a73aa31
mataager <omarbussmo077@gmail.com>
2025-06-08 18:27:26 +0300

    Update static.js

--- assets/js/static.js
+++ assets/js/static.js
@@ -20,7 +20,7 @@ const specialshipping = 75;
 const vanishedstock = false;
 const outofstock = true;
 const flippedimages = false;
-const autopricehandle = true;
+const autopricehandle = false;
 const codfees = 10;
 
 // guest checkout
